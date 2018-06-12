1986101c1e3e8caeed5769cb0ebe7bb8ab331b36
karla-descartes <karla@descarteslabs.com>
2018-06-12 13:39:52 -0600

    Finalize ndvi scaling example

--- scale_and_mask_ndvi.py
+++ scale_and_mask_ndvi.py
@@ -1,4 +1,5 @@
 import descarteslabs as dl
+import numpy as np
 
 
 # The bounding box geometry of Nigeria  
@@ -37,28 +38,40 @@ nigeria = {
 
 # Request Modis imagery, which contains indicies that need to be scaled 
 fc = dl.metadata.search(products="modis:09:CREFL", geom=nigeria['geometry'], start_time="2017-05-01", 
-  end_time="2018-05-15", limit=1)
+  end_time="2018-05-15", limit=2)
 
-# Valid and physical ranges of band values can be found in the Catalog band metadata
-valid_range = [0,16000]
-physical_range = [-0.6, 1.0]
+# Fetch the band information using the Metadata API, including the NDVI ranges 
+band_info = dl.metadata.bands("modis:09:CREFL")
+physical_range = []
+valid_range = [] 
+
+for band in band_info:
+  if band["name"] == "ndvi":
+    physical_range = band["physical_range"]
+    valid_range = band["data_range"]
+
+
 
 # Isolate the image IDs to pull data for 
 feat_ids = [feat['id'] for feat in fc['features']]
-print len(feat_ids)
+
 
 # Request the NDVI band and scale it accordingly and the alpha band for masking next 
-data, meta = dl.raster.ndarray(feat_ids, 
-                bands=['ndvi', 'alpha'], 
-                scales=[[valid_range[0], valid_range[1], physical_range[0], physical_range[1]], None],
-                cutline=nigeria['geometry'], data_type='Float32',)
+arr, meta = dl.raster.ndarray(
+        feat_ids,
+        cutline=nigeria['geometry'],
+        bands=['ndvi', 'alpha'],
+        scales=[[valid_range[0], valid_range[1], physical_range[0], physical_range[1]], None],
+        data_type='Float32'
+        )
 
-# Import NumPy to map nodata values to 2, outside the data's valid range 
-import numpy as np
         
-nodata = data[:,:,1] == 0
-masked = np.where(nodata, 2, data[:,:,0])
-print (np.unique(masked, return_counts=True))
+# mask out nodata pixels
+nodata = arr[:,:,-1] == 0
+masked = np.where(nodata, 0, arr[:,:,0])
+print np.unique(masked, return_counts=True)
+
+
 
 
 
