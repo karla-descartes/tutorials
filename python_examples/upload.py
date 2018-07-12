83ebeeb605528f5c9507c18e0ec27defb67ea696
Karla King <karla@descartes.com>
2018-07-12 13:57:58 -0600

    Debug example

--- python_examples/upload.py
+++ python_examples/upload.py
@@ -1,20 +1,23 @@
 from descarteslabs.ext.catalog import catalog
 import descarteslabs as dl 
 
-
+# Create a product entry in our Catalog
 product = catalog.add_product(
-	"simple_image_51",
+	"simple_image",
 	title="Simple Product Creation",
-	description="Blargh",
+	description="An example of creating a product, adding the visible band range, and ingesting a single scene.",
 	)
 
+# Maintain the product id to upload scenes 
 product_id = product['data']['id']
 
 
-
+# Add band information to the product
+# This is a necessary step, and requires the user
+# to know a bit about the data to be ingested
 bands = ['red', 'green', 'blue']
-i = 0
 
+i = 0
 # Get info from catalog or from file 
 for band in bands:
 	i +=1
@@ -32,34 +35,33 @@ for band in bands:
 		)
 
 
-
-geometry = {"type":"Polygon","coordinates":[[[2.165946315534452,48.713171120067045],[2.5359015712706023,48.713171120067045],[2.5359015712706023,48.957687975409726],[2.165946315534452,48.957687975409726],[2.165946315534452,48.713171120067045]]]}
+# Set an area of interest and search for Sentinel imagery
+paris = {"type":"Polygon","coordinates":[[[2.165946315534452,48.713171120067045],[2.5359015712706023,48.713171120067045],[2.5359015712706023,48.957687975409726],[2.165946315534452,48.957687975409726],[2.165946315534452,48.713171120067045]]]}
 
 
 scenes_meta = dl.metadata.search(product='sentinel-2:L1C',
                               start_time='2018-06-24',
                               end_time='2018-06-30',
-                              geom=geometry,
+                              geom=paris,
                               limit=2,
                               cloud_fraction=.1)
 
 image_ids = [img['id'] for img in scenes_meta['features']]
 
 
-
+# Request a mosaic of the data as a NumPy area
 img_data, meta = dl.raster.ndarray(
 	image_ids,
 	bands=['red', 'green', 'blue'],
 	resolution=20,
-	cutline=geometry
+	cutline=paris
 )
 
+# Upload the ndarray as a single scene in our new product
 catalog.upload_ndarray(
 	img_data,
 	product_id=product_id,
 	image_id="Paris",
 	raster_meta=meta)
-
-
 
 
